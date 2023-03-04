5793e459c21b114f950127b97915604f4bccb6f8
Anton Larin <edding.default@gmail.com>
2023-03-04 19:46:41 +0400

    WIP: rewrote some TGA functions from C++

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod tgaimage;
 
-use tgaimage::TGAColor;
+use tgaimage::{TGAColor, TGAImage, RGB};
+
+const WHITE: TGAColor = TGAColor::from_components(255, 255, 255, 255);
+const RED: TGAColor = TGAColor::from_components(255, 0, 0, 255);
 
 fn main() {
-    let color = TGAColor::new();
-    println!("{:?}", color);
-    println!("Hello, world!");
+    let mut image = TGAImage::with_size(100, 100, RGB);
+    image.set(52, 41, RED);
+    image.flip_vertically();
+    image.write_to_file("output.tga");
 }
--- src/tgaimage.rs
+++ src/tgaimage.rs
@@ -1,8 +1,6 @@
-enum Format {
-    Grayscale = 1,
-    RGB = 3,
-    RGBA = 4,
-}
+pub const GRAYSCALE: i32 = 1;
+pub const RGB: i32 = 3;
+pub const RGBA: i32 = 4;
 
 struct TGAHeader {
     id_length: i8,
@@ -27,14 +25,14 @@ pub struct TGAColor {
 
 impl TGAColor {
     pub fn new() -> TGAColor {
-        return TGAColor {
+        TGAColor {
             val: [0; 4],
             bytespp: 1
         }
     }
 
     pub fn from_components(r: u8, g: u8, b: u8, a: u8) -> TGAColor {
-        return TGAColor {
+        TGAColor {
             val: [b, g, r, a],
             bytespp: 1
         }
@@ -46,7 +44,7 @@ impl TGAColor {
         let g = ((uv >> 16) & 0xFF) as u8;
         let r = ((uv >>  8) & 0xFF) as u8;
         let a = ( uv        & 0xFF) as u8;
-        return TGAColor {
+        TGAColor {
             val: [b, g, r, a],
             bytespp: bpp
         }
@@ -58,7 +56,7 @@ impl TGAColor {
             values[i as usize] = s[i as usize];
         }
 
-        return TGAColor {
+        TGAColor {
             val: values,
             bytespp: bpp
         }
@@ -66,7 +64,7 @@ impl TGAColor {
 }
 
 #[derive(Debug)]
-struct TGAImage {
+pub struct TGAImage {
     data: Vec<u8>,
     width: i32,
     height: i32,
@@ -86,25 +84,52 @@ impl TGAImage {
 
     // }
 
-    // fn new_with_size(w: i32, h: i32, bpp: i32) -> Self {
-
-    // }
+    pub fn with_size(w: i32, h: i32, bpp: i32) -> Self {
+        let size = w * h * bpp;
+        let data = vec![0 as u8; size as usize];
+        TGAImage {
+            data: data,
+            width: w,
+            height: h,
+            bytespp: bpp
+        }
+    }
 
     // fn from_tga_file(filename: &str) -> Option<Self> {
 
     // }
 
-    // fn write_to_file(filename: &str, rle: bool) -> bool {
+    pub fn write_to_file(self: &Self, filename: &str) -> bool {
 
-    // }
+    }
 
     // fn flip_horizontally(self: &mut Self) -> bool {
 
     // }
 
-    // fn flip_vertically(self: &mut Self) -> bool {
+    pub fn flip_vertically(self: &mut Self) -> bool {
+        if self.data.is_empty() {
+            return false
+        }
 
-    // }
+        let bytes_per_line = (self.width * self.bytespp) as usize;
+        let mut line: Vec<u8> = Vec::with_capacity(bytes_per_line);
+
+        let half = (self.height / 2) as usize;
+        for i in 0..half {
+            let l1 = i * bytes_per_line;
+            let l2 = ((self.height - 1 - i as i32) * bytes_per_line as i32) as usize;
+
+            let l1_slice = &mut self.data.as_mut_slice()[l1..(l1 + bytes_per_line)];
+            let l2_slice = &mut self.data.as_mut_slice()[l2..(l2 + bytes_per_line)];
+
+            line.as_mut_slice().copy_from_slice(l1_slice);
+            l1_slice.copy_from_slice(l2_slice);
+            l2_slice.copy_from_slice(line.as_slice());
+        }
+
+        return true
+    }
 
     // fn scale(self: &mut Self, w: i32, h: i32) -> bool {
 
@@ -114,21 +139,17 @@ impl TGAImage {
 
     // }
 
-    // fn set(self: &mut Self, x: i32, y: i32, c: TGAColor) -> bool {
+    pub fn set(self: &mut Self, x: i32, y: i32, c: TGAColor) -> bool {
+        if self.data.is_empty() || x < 0 || y < 0 || x >= self.width || y >= self.height {
+            return false
+        }
 
-    // }
+        let bpp = self.bytespp as usize;
+        let offset = (x + y * self.width) as usize;
+        self.data[offset..(offset + bpp)].copy_from_slice(c.val[0..bpp].as_ref());
 
-    // fn get_width(self: &Self) -> i32 {
-
-    // }
-
-    // fn get_height(self: &Self) -> i32 {
-
-    // }
-
-    // fn get_bytespp(self: &Self) -> i32 {
-
-    // }
+        true
+    }
 
     // fn buffer(self: &Self) -> &[u8] {
     // }
