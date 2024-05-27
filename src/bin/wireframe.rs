ab6f3e2913a2dd03648178711cdb5b17b13cd354
Anton Larin <edding.default@gmail.com>
2024-05-28 01:25:56 +0400

    WIP wireframe render 2; some debug left

--- src/bin/wireframe.rs
+++ src/bin/wireframe.rs
@@ -1,9 +1,9 @@
 extern crate swrender;
 
-use swrender::tgaimage::{tga_format, TGAColor, TGAImage};
-use swrender::objmodel::ObjModel;
-use swrender::math::{Point2, Point3, Vec3};
 use swrender::draw_line;
+use swrender::math::{BndBox2, Point2, Point3, Vec3};
+use swrender::objmodel::ObjModel;
+use swrender::tgaimage::{tga_format, TGAColor, TGAImage};
 
 struct Camera {
     dir: Vec3,
@@ -13,8 +13,8 @@ struct Camera {
 impl Camera {
     fn new(dir: Vec3, up: Vec3) -> Camera {
         let dir = dir.normalize();
-        if let Some(up) = up.normalize().orthogonalize(&dir) {
-            Camera { dir, up }
+        if let Some(up) = up.orthogonalize(dir) {
+            Camera { dir, up: up.normalize() }
         } else {
             panic!("Wrong params for camera given");
         }
@@ -24,20 +24,31 @@ impl Camera {
 /// Projects a 3D point `pnt` onto a plane defined by isometric camera `cam`.
 /// The plane's origin is projection of 3D origin onto it.
 fn project(cam: &Camera, pnt: &Point3) -> Point2 {
-    let mut rad_vec: Vec3 = (*pnt).into();
-    rad_vec = rad_vec - rad_vec.dot(&cam.dir) * rad_vec;
-    // TODO: compute the x and y from camera 'up' direction
-
-    Point2 { x: 0.0, y: 0.0 }
+    let rad_vec: Vec3 = (*pnt).into();
+    let vec_in_plane = rad_vec - rad_vec.dot(cam.dir) * rad_vec;
+    let y = vec_in_plane.dot(cam.up);
+    let x = (vec_in_plane - y * cam.up).norm();
+    Point2 { x, y }
 }
 
 fn main() {
     let white: TGAColor = TGAColor::from_components(255, 255, 255, 255);
 
-    let mut image = TGAImage::with_size(100, 100, tga_format::RGB);
+    const IMAGE_SIZE: i32 = 512;
+    let mut image = TGAImage::with_size(IMAGE_SIZE, IMAGE_SIZE, tga_format::RGB);
     let model = ObjModel::from_file("assets/african_head.obj").unwrap();
-    let camera = Camera::new(Vec3 { x: -1.0, y: -1.0, z: -1.0 },
-                             Vec3 { x: 0.0, y: 0.0, z: 1.0 });
+    let camera = Camera::new(
+        Vec3 {
+            x: -1.0,
+            y: -1.0,
+            z: -1.0,
+        },
+        Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 1.0,
+        },
+    );
 
     // Compute 2D camera plane coords of triangle edges
     let mut pnt2s: Vec<Point2> = Vec::new();
@@ -45,26 +56,39 @@ fn main() {
         let vert1_iter = tri.vertices.into_iter();
         let vert2_iter = tri.vertices.into_iter();
         for (v1, v2) in vert1_iter.zip(vert2_iter.cycle().skip(1)) {
-            let p1 = model.vertices[v1 as usize];
-            let p2 = model.vertices[v2 as usize];
+            let p1 = model.vertices[v1 as usize - 1];
+            let p2 = model.vertices[v2 as usize - 1];
 
             pnt2s.push(project(&camera, &p1));
             pnt2s.push(project(&camera, &p2));
         }
     }
 
-    // TODO: compute 2D bbox for points and set up mapping of camera
-    // plane coords into screen space coords
+    let mut bnd_box = BndBox2::new_empty();
+    for pnt2 in pnt2s.as_slice() {
+        bnd_box.add_point(*pnt2);
+    }
+    let range_vec = bnd_box.max - bnd_box.min;
+    let range = range_vec.x.max(range_vec.y);
+    let center = bnd_box.center();
+    for mut p in pnt2s.as_mut_slice() {
+        p.x = (p.x - center.x) / range * IMAGE_SIZE as f32 + (IMAGE_SIZE / 2) as f32;
+        p.y = (p.y - center.y) / range * IMAGE_SIZE as f32 + (IMAGE_SIZE / 2) as f32;
+    }
 
     // Draw the edges
     for i in (0..pnt2s.len()).step_by(2) {
         let (p1, p2) = (pnt2s[i], pnt2s[i + 1]);
-        draw_line(p1.x as i32, p1.y as i32,
-                  p2.x as i32, p2.y as i32,
-                  &mut image, white);
+        draw_line(
+            p1.x as i32,
+            p1.y as i32,
+            p2.x as i32,
+            p2.y as i32,
+            &mut image,
+            white,
+        );
     }
 
     image.flip_vertically().unwrap();
-    image.write_to_file("output.tga").unwrap();
+    image.write_to_file("assets/output.tga").unwrap();
 }
-
--- src/math.rs
+++ src/math.rs
@@ -8,6 +8,28 @@ pub struct Point2 {
     pub y: f32,
 }
 
+impl Point2 {
+    pub fn origin() -> Self {
+        Self { x: 0.0, y: 0.0 }
+    }
+}
+
+impl Sub for Point2 {
+    type Output = Vec2;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::Output { x: self.x - rhs.x, y: self.y - rhs.y }
+    }
+}
+
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Vec2 {
+}
+
 #[derive(Clone, Copy)]
 pub struct Point3 {
     pub x: f32,
@@ -23,10 +45,6 @@ impl Point3 {
     pub fn from_slice(coord: &[f32]) -> Self {
         assert!(coord.len() == 3);
         Point3 { x: coord[0], y: coord[1], z: coord[2] }
-    }
-
-    pub fn add(&self, vec: Vec3) -> Self {
-        Point3 { x: self.x + vec.x, y: self.y + vec.y, z: self.z + vec.z }
     }
 
     pub fn to_vec3(&self) -> Vec3 {
@@ -48,7 +66,7 @@ pub struct Vec3 {
 }
 
 impl Vec3 {
-    pub fn dot(&self, other: &Self) -> f32 {
+    pub fn dot(&self, other: Self) -> f32 {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
@@ -62,9 +80,9 @@ impl Vec3 {
         *self * norm.recip()
     }
 
-    pub fn orthogonalize(&self, ref_vec: &Self) -> Option<Self> {
+    pub fn orthogonalize(&self, ref_vec: Self) -> Option<Self> {
         assert!(ref_vec.norm() > EPSILON);
-        let collinear_portion = self.dot(ref_vec) * *ref_vec;
+        let collinear_portion = self.dot(ref_vec) * ref_vec;
         let res = *self - collinear_portion;
         if res.norm() < EPSILON {
             None
@@ -73,6 +91,8 @@ impl Vec3 {
         }
     }
 }
+
+// TODO: add impl Add<Point3> for Vec3 and Add<Vec3> for Point3
 
 impl From<Point3> for Vec3 {
     fn from(p: Point3) -> Self {
@@ -109,6 +129,29 @@ impl Mul<Vec3> for f32 {
 
     fn mul(self, rhs: Vec3) -> Self::Output {
         Self::Output { x: self * rhs.x, y: self * rhs.y, z: self * rhs.z }
+    }
+}
+
+pub struct BndBox2 {
+    pub min: Point2,
+    pub max: Point2,
+}
+
+impl BndBox2 {
+    pub fn new_empty() -> Self {
+        Self { min: Point2::origin(), max: Point2::origin() }
+    }
+
+    pub fn add_point(&mut self, pnt: Point2) {
+        if pnt.x < self.min.x { self.min.x = pnt.x; }
+        else if pnt.x > self.max.x { self.max.x = pnt.x; }
+
+        if pnt.y < self.min.y { self.min.y = pnt.y; }
+        else if pnt.y > self.max.y { self.max.y = pnt.y; }
+    }
+
+    pub fn center(&self) -> Point2 {
+        Point2 { x: 0.5 * (self.min.x + self.max.x), y: 0.5 * (self.min.y + self.max.y) }
     }
 }
 
