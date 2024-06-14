94198c6f6bae00029b759d1320f7a1023ff8c164
Anton Larin <edding.default@gmail.com>
2024-06-14 21:31:27 +0400

    Finalize naive triangle rendering on algo from course

--- src/renderer/triangle.rs
+++ src/renderer/triangle.rs
@@ -2,44 +2,36 @@ use crate::math::Point2i;
 use crate::tgaimage::{TGAColor, TGAImage};
 
 pub fn draw_triangle(v1: Point2i, v2: Point2i, v3: Point2i, image: &mut TGAImage, color: TGAColor) {
-    let (mut p1, mut p2, mut p3) = (v1, v2, v3);
-    if p2.y < p1.y { std::mem::swap(&mut p1, &mut p2); }
-    if p3.y < p2.y { std::mem::swap(&mut p2, &mut p3); }
-    if p2.y < p1.y { std::mem::swap(&mut p1, &mut p2); }
-
-    // TODO: fix x-degen triangle
-    // TODO: fix corner tests
-    let mut lmul = if p2.y == p1.y { (1, 1) } else { (p2.x - p1.x, p2.y - p1.y) };
-    let mut rmul = if p3.y == p1.y { (1, 1) } else { (p3.x - p1.x, p3.y - p1.y) };
-    {
-        let ltest = p1.x + 1 * lmul.0 / lmul.1;
-        let rtest = p1.x + 1 * rmul.0 / rmul.1;
-        if ltest > rtest {
-            std::mem::swap(&mut lmul, &mut rmul);
-        }
-    }
-    for y in p1.y..p2.y {
-        let xl = p1.x + (y - p1.y) * lmul.0 / lmul.1;
-        let xr = p1.x + (y - p1.y) * rmul.0 / rmul.1;
+    // handle degenerate triangle first
+    if v1.y == v2.y && v2.y == v3.y {
+        let xl = v1.x.min(v2.x.min(v3.x));
+        let xr = v1.x.max(v2.x.max(v3.x));
         for x in xl..=xr {
-            image.set(x, y, color).unwrap();
+            image.set(x, v1.y, color).unwrap();
         }
+        return;
     }
 
-    lmul = if p1.y == p3.y { (1, 1) } else { (p1.x - p3.x, p1.y - p3.y) };
-    rmul = if p2.y == p3.y { (1, 1) } else { (p2.x - p3.x, p2.y - p3.y) };
-    {
-        let ltest = p3.x - 1 * lmul.0 / lmul.1;
-        let rtest = p3.x - 1 * rmul.0 / rmul.1;
-        if ltest > rtest {
-            std::mem::swap(&mut lmul, &mut rmul);
-        }
-    }
-    for y in p2.y..=p3.y {
-        let xl = p3.x + (y - p3.y) * lmul.0 / lmul.1;
-        let xr = p3.x + (y - p3.y) * rmul.0 / rmul.1;
+    let mut vs = [v1, v2, v3];
+    vs.sort_by_key(|v| v.y);
+    let (p1, p2, p3) = (vs[0], vs[1], vs[2]);
+
+    let total_height = p3.y - p1.y;
+    for i in 0..=total_height {
+        let second_half = i > p2.y - p1.y || p1.y == p2.y;
+        let segment_height = if second_half { p3.y - p2.y } else { p2.y - p1.y };
+        let local_i = if second_half { i - p2.y + p1.y } else { i };
+
+        let mut xl = p1.x + i * (p3.x - p1.x) / total_height;
+        let mut xr = if !second_half {
+            p1.x + i * (p2.x - p1.x) / segment_height
+        } else {
+            p2.x + local_i * (p3.x - p2.x) / segment_height
+        };
+
+        if xl > xr { std::mem::swap(&mut xl, &mut xr); }
         for x in xl..=xr {
-            image.set(x, y, color).unwrap();
+            image.set(x, p1.y + i, color).unwrap();
         }
     }
 }
@@ -75,14 +67,12 @@ mod test {
 
     #[test]
     fn degenerate_x_triangle() {
-        let mut img = TGAImage::with_size(32, 32, tga_format::RGB);
-        let col = TGAColor::from_rgb(255, 255, 255);
-        // let (mut img, col) = setup_1_image();
+        let (mut img, col) = setup_1_image();
         let black = TGAColor::from_rgb(0, 0, 0);
 
-        let v1 = Point2i { x: 0, y: 16 };
-        let v2 = Point2i { x: 14, y: 16 };
-        let v3 = Point2i { x: 31, y: 16 };
+        let v1 = Point2i { x: 0, y: 3 };
+        let v2 = Point2i { x: 2, y: 3 };
+        let v3 = Point2i { x: 5, y: 3 };
 
         draw_triangle(v1, v2, v3, &mut img, col);
         img.write_to_file("assets/test_degen_x.tga").unwrap();
